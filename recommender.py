ce31fc3ab5efbbe9208001bf98fda1712e8bcaa1
Syed Islam <syedm@ucr-secure-29-10-13-165-21.wnet.ucr.edu>
2025-02-22 21:53:33 -0800

    Merged sampler into recommender file to execute all in 1 python script

--- recommender.py
+++ recommender.py
@@ -1,9 +1,27 @@
 import numpy as np
+import pandas as pd
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-#This function computes TF-IDf vectors
+# This function samples and preprocesses the dataset
+def sample_and_preprocess_dataset():
+    # Load the dataset
+    df = pd.read_csv('Data/movies_metadata.csv')
+    
+    # Extract only the necessary columns
+    subset_df = df[['original_title', 'overview']]
+    
+    # Sample 500 rows from the dataset
+    sample_subset_df = subset_df.sample(n=500, random_state=42)
+    
+    # Save the sampled dataset
+    sample_subset_df.to_csv('Data/sampled_movies.csv', index=False)
+    
+    return
+
+
+# This function computes TF-IDf vectors
 def compute_idf_vectors(query, documents):
     # Initialize the TF-IDF Vectorizer
     vectorizer = TfidfVectorizer(stop_words='english')
@@ -14,7 +32,7 @@ def compute_idf_vectors(query, documents):
     
     return movie_vector, query_vector
 
-#This function computes cosine similarity between query and movies
+# This function computes cosine similarity between query and movies
 def get_cosine_similarity(query_vector, movie_vector):
     
     cosine_sim = cosine_similarity(query_vector, movie_vector)
@@ -22,6 +40,8 @@ def get_cosine_similarity(query_vector, movie_vector):
     return cosine_sim
 
 def main():
+    #Preprocess the dataset
+    sample_and_preprocess_dataset()
     # Load the sampled dataset
     file_path = 'Data/sampled_movies.csv'
     with open(file_path, "r", encoding="utf-8") as file:
